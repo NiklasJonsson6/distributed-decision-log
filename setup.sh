e5f9d1c4c2bf13d82edf1bf14c68c31c660e3247
Niklas Jonsson <niklas.jonsson@yolean.com>
2020-06-02 14:58:57 +0200

    channel-flow for setting up channels, currently fails a typecheck on docker.sock

--- setup.sh
+++ setup.sh
@@ -29,3 +29,9 @@ echo "-- collect the host aliases --"
 echo "-- then update the network with host aliases --"
 helm upgrade hlf-kube ./hlf-kube -f $project_folder/network.yaml -f $project_folder/crypto-config.yaml \
 -f $project_folder/hostAliases.yaml --set orderer.cluster.enabled=true 
+
+echo "-- waiting for all pods to start --"
+kubectl wait --for condition=ready pods --all
+
+echo "-- create the channel(s) --"
+helm template channel-flow/ -f $project_folder/network.yaml -f $project_folder/crypto-config.yaml -f $project_folder/hostAliases.yaml | argo submit - --watch
