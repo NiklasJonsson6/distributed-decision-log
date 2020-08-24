f9b14c107cbf2bc6ff1ea0ea20db825fbae37555
Niklas Jonsson <niklas.jonsson@yolean.com>
2020-08-24 16:00:18 +0200

    all transactions working

--- chaincode/decision-log-shim/decision.js
+++ chaincode/decision-log-shim/decision.js
@@ -5,24 +5,127 @@ const {logger} = require('@kunstmaan/hyperledger-fabric-node-chaincode-utils').u
 const myLogger = logger.getLogger('myLogger');
 const ChaincodeBase = require('@kunstmaan/hyperledger-fabric-node-chaincode-utils').ChaincodeBase;
 
+const states = {
+  PENDING: 'pending',
+  ONGOING: 'ongoing',
+  ENDED: 'ended'
+};
+
 const Decision = class extends ChaincodeBase {
   constructor() {
     super(shim);
   }
 
-  async startDecision(stub, txHelper) {
-    const queryString = {
-      selector: {}
-    };
-    const n = await txHelper.getQueryResultAsList(queryString);
-    const decisionId = n.toString();
-    myLogger.info(`startDecision returned id: ${n.length}`);
-    return decisionId;
+
+  /**
+   * Retreives the given decision's current state
+   * @param {string} decision_id
+   * @returns {Object} the decision
+   */
+  async queryDecision(stub, txHelper, decision_id) {
+    const decision = await txHelper.getStateAsObject(decision_id);
+    myLogger.info(`Queried decision with id: ${decision_id}, ${JSON.stringify(decision)}`);
+    return decision;
   }
 
-  async join(stub, txHelper, client_id) {
-    myLogger.info(`join() called by ${client_id}`);
-    return 'join called';
+  /**
+   * Starts a decision as pending
+   * @param {string} client_id the initiating client
+   * @returns {string} decision_id
+   */
+  async startDecision(stub, txHelper, client_id, decision_id = txHelper.uuid('decision')) {
+    //const decision_id = t;
+    const decision = {
+      decision_id: decision_id,
+      state: states.PENDING,
+      attendance: [
+        {
+          client_id: client_id,
+          reservation: false,
+          hereAtStart: false,
+          hereAtEnd: false
+        }
+      ]
+    };
+    await txHelper.putState(decision_id, decision);
+    myLogger.info(`Decision with id ${decision_id} added to ledger`);
+    return decision_id;
+  }
+
+  /**
+   * Joins a pending decision
+   * @param {string} decision_id
+   * @param {string} client_id
+   */
+  async join(stub, txHelper, client_id, decision_id) {
+    const decision = await txHelper.getStateAsObject(decision_id);
+    if (decision.state !== states.PENDING) {
+      throw new Error(`Decision state is ${decision.state}, should be ${states.PENDING}`);
+    }
+    if (decision.attendance.filter(client => client.client_id === client_id).length > 0) {
+      throw new Error(`Client with id: ${client_id} is already joined to the decision`);
+    }
+    decision.attendance.push({
+      client_id: client_id,
+      reservation: false,
+      hereAtStart: false,
+      hereAtEnd: false
+    });
+    await txHelper.putState(decision_id, decision);
+    myLogger.info(`Client with id: ${client_id} joined decision with id: ${decision_id}`);
+  }
+
+  /**
+   * Sets a decision from pending to ongoing
+   * @param {string} decision_id the decision to start
+   * @returns "success" if state was set successfully
+   */
+  async setOngoing(stub, txHelper, decision_id) {
+    const decision = await txHelper.getStateAsObject(decision_id);
+    if (decision.state !== states.PENDING) {
+      throw new Error(`Decision state is ${decision.state}, should be ${states.PENDING}`);
+    }
+    decision.state = states.ONGOING;
+    await txHelper.putState(decision_id, decision);
+    myLogger.info(`Decision with id: ${decision_id} set to ${states.ONGOING}`);
+    return 'success';
+  }
+
+  /**
+   * Asserts the client's participation
+   * @param {string} client_id
+   * @param {string} decision_id
+   */
+  async here(stub, txHelper, client_id, decision_id) {
+    const decision = await txHelper.getStateAsObject(decision_id);
+    const index = decision.attendance.findIndex(client => client.client_id === client_id);
+    if (index === -1) {
+      throw new Error(`Client with id: ${client_id} is not in attendance`);
+    }
+    if (decision.state === states.ONGOING) {
+      decision.attendance[index].hereAtStart = true;
+    } else if (decision.state === states.ENDED) {
+      decision.attendance[index].hereAtEnd = true;
+    } else {
+      throw new Error(`Decision with id: ${decision_id} is in state ${decision.state}, should be ${states.ONGOING} or ${states.ENDED}`);
+    }
+    await txHelper.putState(decision_id, decision);
+    myLogger.info(`Client with id: ${client_id} announced its participation`);
+  }
+
+  /**
+   * Ends the ongoing phase of a decision
+   * @param {string} decision_id
+   */
+  async setEnded(stub, txHelper, decision_id) {
+    const decision = await txHelper.getStateAsObject(decision_id);
+    if (decision.state !== states.ONGOING) {
+      throw new Error(`Decision state is ${decision.state}, should be ${states.ONGOING}`);
+    }
+    decision.state = states.ENDED;
+    await txHelper.putState(decision_id, decision);
+    myLogger.info(`Decision with id: ${decision_id} set to ${states.ENDED}`);
+    return 'success';
   }
 };
 
--- chaincode/decision-log-shim/test/decision.js
+++ chaincode/decision-log-shim/test/decision.js
@@ -2,6 +2,7 @@
 
 const Decision = require('../decision');
 const ChaincodeMockStub = require('@theledger/fabric-mock-stub').ChaincodeMockStub;
+const Transform = require('@theledger/fabric-mock-stub').Transform;
 const expect = require('chai').expect;
 
 const decision = new Decision();
@@ -13,9 +14,34 @@ describe('Test Chaincode', () => {
     expect(response.status).to.eql(200);
   });
 
-  it('Should return the nubmer of assets', async () => {
+  it('Should work...', async () => {
+    // Simply test if the transactions go through, the actual response should not matter in most cases
     const stub = new ChaincodeMockStub('MockStub', decision);
-    const response = await stub.mockInvoke('tx2', ['startDecision']);
-    expect(response.status).to.eql(200);
+    const response = await stub.mockInvoke('tx1', ['startDecision', 'client_id0', 'decision_id0']);
+    expect(Transform.bufferToObject(response.payload)).to.deep.eql('decision_id0');
+
+    // query
+    const resp_query = await stub.mockInvoke('tx2', ['queryDecision', 'decision_id0']);
+    expect(resp_query.status).to.eql(200);
+
+    // join
+    const resp_join = await stub.mockInvoke('tx3', ['join', 'client_id1', 'decision_id0']);
+    expect(resp_join.status).to.eql(200);
+
+    // setOngoing
+    const resp_setOngoing = await stub.mockInvoke('tx4', ['setOngoing', 'decision_id0']);
+    expect(resp_setOngoing.status).to.eql(200);
+
+    // here
+    const resp_here = await stub.mockInvoke('tx5', ['here', 'client_id1', 'decision_id0']);
+    expect(resp_here.status).to.eql(200);
+
+    // setEnded
+    const resp_setEnded = await stub.mockInvoke('tx4', ['setEnded', 'decision_id0']);
+    expect(resp_setEnded.status).to.eql(200);
+
+    // here2
+    const resp_here2 = await stub.mockInvoke('tx5', ['here', 'client_id1', 'decision_id0']);
+    expect(resp_here2.status).to.eql(200);
   });
 });
