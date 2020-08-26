f3887bd473c8e83f1965e73ad2074453bc4b2b3a
Niklas Jonsson <niklas.jonsson@yolean.com>
2020-08-26 13:33:19 +0200

    some backend functionality

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -1,19 +1,60 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
 const port = 8080;
 
-const enrollAdmin = require('./enrollAdmin');
-const ping = require('./ping');
+const enrollAdmin = require('./enroll-admin');
+const submitTransaction = require('./submit-transaction');
 
-async function main() {
-  const walletPath = await enrollAdmin.enroll();
-  ping(walletPath);
-}
+// Client API
+app.get('/startDecision', async (req, res) => {
+  const transactionResponse = await submitTransaction(await enrollAdmin.enroll(), 'startDecision', uuidv4());
+  app.locals.decisionId = transactionResponse.toString('utf8');
+  console.log(`Decision started with id: ${app.locals.decisionId}`);
+  res.sendStatus(200);
+});
 
-app.get('/', (req, res) => res.send(main()));
+app.get('/here', async (req, res) => {
+  if (!('decisionId' in app.locals)) {
+    console.log('No decision id found, start a decision first!');
+    res.sendStatus(404);
+  } else {
+    if (!(req.ip in app.locals)) {
+      app.locals[req.ip] = uuidv4();
+    }
+    const clientId = app.locals[req.ip];
+
+    await submitTransaction(await enrollAdmin.enroll(), 'here', clientId, app.locals.decisionId);
+    res.sendStatus(200);
+  }
+});
+
+app.get('/reserve', async (req, res) => {
+  if (!('decisionId' in app.locals)) {
+    console.log('No decision id found, start a decision first!');
+    res.sendStatus(404);
+  } else {
+    if (!(req.ip in app.locals)) {
+      throw new Error('reserve call from non-participating client');
+    }
+    const clientId = app.locals[req.ip];
+
+    await submitTransaction(await enrollAdmin.enroll(), 'reserve', clientId, app.locals.decisionId);
+    res.sendStatus(200);
+  }
+});
+
+// Control/test functions
+app.post('/', (req, res) => {
+  if (req.ip in app.locals) {
+    console.log('key in locals!');
+  } else {
+    app.locals[req.ip] = uuidv4();
+  }
+  console.log(app.locals);
+  res.send();
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
-
-main();
--- backend-node/src/submit-transaction.js
+++ backend-node/src/submit-transaction.js
@@ -4,7 +4,13 @@ const { Wallets, Gateway } = require('fabric-network');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-const ping = async (walletPath) => {
+/**
+ * Submit a transaction to the ledger
+ * @param {string} walletPath
+ * @param {string} name
+ * @param  {...string} args
+ */
+const submitTransaction = async (walletPath, name, ...args) => {
   const gateway = new Gateway();
   const wallet = await Wallets.newFileSystemWallet(walletPath);
   try {
@@ -17,14 +23,19 @@ const ping = async (walletPath) => {
     console.log('connecting to gateway');
     await gateway.connect(yaml.safeLoad(fs.readFileSync('src/resources/connection-yolean.yaml', 'utf8')), connectionOptions);
     const network = await gateway.getNetwork('meeting1');
-    const contract = await network.getContract('decision-log-shim');
-    console.log('submitting transaction');
-    const response = await contract.submitTransaction('ping');
+    const contract = network.getContract('decision-log-shim');
+
+    console.log(`submitting transaction: ${name} with args: ${args}`);
+    const response = await contract.submitTransaction(name, args);
     console.log(`response: ${response}`);
+    return response;
   } catch (error) {
-    console.log(`Error on ping: ${error}`);
+    console.log(`Error on transaction: ${error}`);
     console.log(error.stack);
+    throw error;
+  } finally {
+    gateway.disconnect();
   }
 };
 
-module.exports = ping;
+module.exports = submitTransaction;
